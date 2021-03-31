cb34cb98ebd140d0ac093192451a4793a80ac93d
Plamen <pyvelkov@myseneca.ca>
2021-03-31 09:23:23 -0700

    Initial Commit

---
+++ Plamen_Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plamen_Weather
+//
+//  Created by Plamen Velkov on 2021-03-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
